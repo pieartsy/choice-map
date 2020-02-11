4bd64d39b4dd1df11403dc2667aab31dc927f620
pieartsy <pieartsy@users.noreply.github.com>
2020-02-10 21:52:48 -0500

    added drag and drop edge creation
    bug: can't undo edge creation

--- cygraph.js
+++ cygraph.js
@@ -1,12 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+
 var cy = cytoscape({
     container: document.getElementById("cy"),
 elements: {
     nodes: [
-      { data: { id: 'a', name: "atest"}, classes: "atest"},
-      { data: { id: 'b', name: "btest"}, classes: "btest" },
-      { data: { id: 'c', name: "ctest" }, classes: "ctest" },
-      { data: { id: 'd', name: "dtest" }, classes: "dtest" },
-      { data: { id: 'e', name: "etest" }, classes: "etest" },
+      { data: { id: 'a', name: "atest"} },
+      { data: { id: 'b', name: "btest"} },
+      { data: { id: 'c', name: "ctest" } },
+      { data: { id: 'd', name: "dtest" } },
+      { data: { id: 'e', name: "etest" } },
     ],
     edges: [
       { data: { source: 'a', target: 'b' } },
@@ -20,8 +22,45 @@ elements: {
   //selectors for undo/redo
   style: [
     { selector: 'node', style: { 'content': 'data(name)' }  },
-    { selector: 'edge', style: { 'target-arrow-shape': 'triangle' } },
-    { selector: ':selected', style: {  } }
+    { selector: 'edge', style: {  'curve-style': 'bezier', 'target-arrow-shape': 'triangle' } },
+    { selector: ':selected', style: {  } },
+    // some style for endge handler
+    { selector: '.eh-handle',
+        style: {
+          'background-color': 'red',
+          'width': 12,
+          'height': 12,
+          'shape': 'ellipse',
+          'overlay-opacity': 0,
+          'border-width': 12, // makes the handle easier to hit
+          'border-opacity': 0
+        }
+    },
+    { selector: '.eh-hover',  style: { 'background-color': 'red' } },
+    { selector: '.eh-source',
+        style: {
+          'border-width': 2,
+          'border-color': 'red'
+        }
+    },
+    { selector: '.eh-target',
+        style: {
+          'border-width': 2,
+          'border-color': 'red'
+        }
+    },
+
+    { selector: '.eh-preview, .eh-ghost-edge',
+        style: {
+          'background-color': 'red',
+          'line-color': 'red',
+          'target-arrow-color': 'red',
+          'source-arrow-color': 'red'
+        }
+    },
+
+    { selector: '.eh-ghost-edge.eh-preview-active',
+      style: { 'opacity': 0 } }
     ],
 
   layout: {
@@ -67,4 +106,9 @@ document.addEventListener("click", function(e){
         y: e.clientY,
       },
     });
+});
+
+
+var eh = cy.edgehandles({});
+
 });
