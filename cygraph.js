a7f92797ed9595bf6923c7a60a69459dd41e3dde
pieartsy <pieartsy@users.noreply.github.com>
2020-02-10 22:13:04 -0500

    adds documentation

--- cygraph.js
+++ cygraph.js
@@ -1,8 +1,12 @@
+//waits until DOM is loaded for script to run
 document.addEventListener('DOMContentLoaded', function(){
 
+//declares cytoscape graph with container as the div id 'cy' in index.html
 var cy = cytoscape({
     container: document.getElementById("cy"),
+//the parts of the graph
 elements: {
+  //the circles with labels
     nodes: [
       { data: { id: 'a', name: "atest"} },
       { data: { id: 'b', name: "btest"} },
@@ -10,6 +14,7 @@ elements: {
       { data: { id: 'd', name: "dtest" } },
       { data: { id: 'e', name: "etest" } },
     ],
+    //the lines connecting them
     edges: [
       { data: { source: 'a', target: 'b' } },
       { data: { source: 'b', target: 'c' } },
@@ -19,12 +24,16 @@ elements: {
       { data: { source: 'b', target: 'e' } },
     ]
   },
-  //selectors for undo/redo
+  //styles
   style: [
+    //adds the name attribute (the node's label, ex: 'btest') to the node
     { selector: 'node', style: { 'content': 'data(name)' }  },
+    //styles the edges as having directed arrows and bezier curves
     { selector: 'edge', style: {  'curve-style': 'bezier', 'target-arrow-shape': 'triangle' } },
+    //a selector for undo/redo
     { selector: ':selected', style: {  } },
-    // some style for endge handler
+    // some style for edge handler
+    //for the little dot that you grab to make an edge
     { selector: '.eh-handle',
         style: {
           'background-color': 'red',
@@ -36,7 +45,9 @@ elements: {
           'border-opacity': 0
         }
     },
+    //when you hover over the target node with your handle, it turns red
     { selector: '.eh-hover',  style: { 'background-color': 'red' } },
+    //when you grab the edge handle and move it away from the given node, its border is red
     { selector: '.eh-source',
         style: {
           'border-width': 2,
@@ -49,7 +60,7 @@ elements: {
           'border-color': 'red'
         }
     },
-
+    //the 'ghost' (not locked in) edges are red
     { selector: '.eh-preview, .eh-ghost-edge',
         style: {
           'background-color': 'red',
@@ -62,7 +73,7 @@ elements: {
     { selector: '.eh-ghost-edge.eh-preview-active',
       style: { 'opacity': 0 } }
     ],
-
+//makes the layout a directed tree graph
   layout: {
     name: 'breadthfirst',
     directed: true,
@@ -77,27 +88,34 @@ var b = cy.$('#b');
 console.log(a.edgesWith(b));
 
 
-//undo/redo
+//undo/redo extension
 
 var ur = cy.undoRedo ({
   });
 
+//listens for a keydown
 document.addEventListener("keydown", function (e) {
+  //if keydown is delete button, remove element
     if(e.which === 46) {
       var selecteds = cy.$(":selected");
     if (selecteds.length > 0)
       ur.do("remove", selecteds);
     }
-
+    //if keydown is ctrl [dunno what the stuff after && is]
     else if (e.ctrlKey && e.target.nodeName === 'BODY')
+    //and z, undo
       if (e.which === 90)
         ur.undo();
+    //and y, redo
     else if (e.which === 89)
       ur.redo();
 });
 
+//listens for a mouse click
 document.addEventListener("click", function(e){
+  //if alt key is pressed at the same time
   if (e.altKey)
+  //add a node where the mouse is
     ur.do("add", {
       group: "nodes",
       data: { weight: 75, name: "New Node" },
@@ -108,7 +126,7 @@ document.addEventListener("click", function(e){
     });
 });
 
-
+//edge handles extension
 var eh = cy.edgehandles({});
 
 });
